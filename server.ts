82df508e10be28108a9380b729f5470ef90b67dc
Rajanish-code <141335580+Rajanish-code@users.noreply.github.com>
2024-10-16 12:41:58 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -3,18 +3,20 @@ import studentRouter from "./routes/studentRoutes.ts";
 import courseRouter from "./routes/courseRoutes.ts";
 import enrollmentRouter from "./routes/enrollmentRoutes.ts";
 import { oakCors } from "./deps.ts"; 
+
 const app = new Application();
 
-
+// CORS middleware
 app.use(
     oakCors({
-      origin: "*", // Allow all origins
-      methods: ["GET", "POST", "PATCH", "DELETE"],
-      allowedHeaders: ["Authorization", "Content-Type"],
-      credentials: true, // Allow cookies and credentials
+        origin: "*", // Allow all origins
+        methods: ["GET", "POST", "PATCH", "DELETE"],
+        allowedHeaders: ["Authorization", "Content-Type"],
+        credentials: true, // Allow cookies and credentials
     }),
-  );
+);
 
+// Register routers
 app.use(studentRouter.routes());
 app.use(studentRouter.allowedMethods());
 
@@ -24,5 +26,8 @@ app.use(courseRouter.allowedMethods());
 app.use(enrollmentRouter.routes());
 app.use(enrollmentRouter.allowedMethods());
 
-console.log("Server is running on http://localhost:8000");
-await app.listen({ port: 8000 });
+// Get the port from the environment variable or use 8000 as the default
+const port = Number(Deno.env.get("PORT")) || 8000;
+
+console.log(`Server is running on http://localhost:${port}`);
+await app.listen({ port });
